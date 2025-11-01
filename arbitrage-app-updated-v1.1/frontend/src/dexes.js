80c7335603d60307cbed2212f13f9616d2baf5cc
UmarfarouqRb <umafarouqrb@gmail.com>
2025-11-01 23:42:06 +0100

    arbitrage-app

--- arbitrage-app-updated-v1.1/frontend/src/dexes.js
+++
@@ -1,8 +0,0 @@
-
-export const FACTORIES = [
-  { name: 'UniswapV2', chain: 'Ethereum', factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' },
-  { name: 'SushiSwap', chain: 'Ethereum', factory: '0xC0AEe478e3658e2610c5F7A4A2E1777Ce9e4f2Ac', router: '0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f' },
-  { name: 'PancakeV2', chain: 'BSC', factory: '0xca143ce32fe78f1f7019d7d551a6402fc5350c73', router: '0x10ED43C718714eb63d5aA57B78B54704E256024E' },
-  { name: 'QuickSwap', chain: 'Polygon', factory: '0x5757371414417b8c6caad45bae9414bpc7d3ab32', router: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff' },
-  { name: 'BistroSwap', chain: 'Example', factory: '0x0000000000000000000000000000000000000000', router: '0x0000000000000000000000000000000000000000' }
-];
