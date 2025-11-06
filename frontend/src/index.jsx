e09a72f71452c8a38990b7ed7ae367dc2f34bde4
umarfarouqrb <umarfarouqrb@gmail.com>
2025-11-06 07:10:06 +0000

    fix: Wrap App in ThirdwebProvider to fix rendering issue

---
+++ frontend/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import {ThirdwebProvider} from 'thirdweb/react';
+
+createRoot(document.getElementById('root')).render(
+<ThirdwebProvider>
+<App />
+</ThirdwebProvider>
+);
